d2974d27c90ccc66c660792348099beaa9374005
sun-one95 <isay0693@gmail.com>
2021-11-19 23:51:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const increase = document.getElementById('increase');
 const decrease = document.getElementById('decrease');
 
 increase.onclick = () => {
-    const current = parseInt(number.innerText, 10);
+    const current = parseInt(number.innerText, 10)
     number.innerText = current + 1
 }
 
 decrease.onclick = () => {
-    const current = parseInt(number.innerText, 10);
+    const current = parseInt(number.innerText, 10)
     number.innerText = current - 1
 }
 
